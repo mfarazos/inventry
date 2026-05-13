d8ca5a166eaa17f7af71cb4cb24c35e32fe1d729
mfarazos <56586227+mfarazos@users.noreply.github.com>
2026-05-13 18:33:59 +0500

    done

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -10,7 +10,8 @@ export type AppConfig = {
 
 const appConfig: AppConfig = {
        //apiPrefix: "https://apps.leadsmovinghomecompany.com",
-       apiPrefix: "http://localhost:3004",
+       //apiPrefix: "http://localhost:3004",
+       apiPrefix: "https://nooraniplastic.com",
       //  apiPrefix: import.meta.env.VITE_BASE_URL,
   authenticatedEntryPath: "/dashboard",
   unAuthenticatedEntryPath: "/sign-in",
